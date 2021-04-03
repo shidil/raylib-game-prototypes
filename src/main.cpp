61be780767f45cad1423253468d66d8c3d275280
Shidil Eringa <shidil@live.com>
2021-04-03 16:01:57 +0530

    Snake prototoype

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,110 @@
 /*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+ *
+ *   raylib [core] example - Basic 3d example
+ *
+ *   Welcome to raylib!
+ *
+ *   To compile example, just press F5.
+ *   Note that compiled executable is placed in the same folder as .c file
+ *
+ *   You can find all basic examples on C:\raylib\raylib\examples folder or
+ *   raylib official webpage: www.raylib.com
+ *
+ *   Enjoy using raylib. :)
+ *
+ *   This example has been created using raylib 1.0 (www.raylib.com)
+ *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h
+ *for details)
+ *
+ *   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
+ *
+ ********************************************************************************************/
 
 #include <raylib.h>
 
-int main()
-{
+#include <vector>
+
+using namespace std;
+
+void drawSnake(vector<Vector2>);
+void moveSnake(vector<Vector2> &currentSnake, Vector2 direction);
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 450;
+const int SNAKE_SCALE = 5;
+
+int main() {
   // Initialization
   //--------------------------------------------------------------------------------------
-  const int screenWidth = 800;
-  const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib");
+  Camera2D camera = {0};
+  camera.rotation = 0.0f;
+  camera.zoom = 1.0f;
 
-  Camera camera = {0};
-  camera.position = (Vector3){10.0f, 10.0f, 8.0f};
-  camera.target = (Vector3){0.0f, 0.0f, 0.0f};
-  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-  camera.fovy = 60.0f;
-  camera.projection = CAMERA_PERSPECTIVE;
-
-  SetCameraMode(camera, CAMERA_ORBITAL);
-
-  Vector3 cubePosition = {0};
-
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib");
+  SetTargetFPS(10);  // Set our game to run at 144 frames-per-second
   //--------------------------------------------------------------------------------------
 
+  vector<Vector2> positions = {
+      (Vector2){2 + SNAKE_SCALE, SCREEN_HEIGHT / 2}, (Vector2){3 + SNAKE_SCALE, SCREEN_HEIGHT / 2},
+      (Vector2){4 + SNAKE_SCALE, SCREEN_HEIGHT / 2}, (Vector2){5 + SNAKE_SCALE, SCREEN_HEIGHT / 2},
+      (Vector2){6 + SNAKE_SCALE, SCREEN_HEIGHT / 2}, (Vector2){7 + SNAKE_SCALE, SCREEN_HEIGHT / 2},
+      (Vector2){8 + SNAKE_SCALE, SCREEN_HEIGHT / 2}, (Vector2){9 + SNAKE_SCALE, SCREEN_HEIGHT / 2},
+  };
+
+  Vector2 direction = {1, 0};
+
   // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    // Update
-    //----------------------------------------------------------------------------------
-    UpdateCamera(&camera);
-    //----------------------------------------------------------------------------------
+  while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_RIGHT)) {
+      direction.x = 1;
+      direction.y = 0;
+    } else if (IsKeyDown(KEY_LEFT)) {
+      direction.x = -1;
+      direction.y = 0;
+    } else if (IsKeyDown(KEY_DOWN)) {
+      direction.y = 1;
+      direction.x = 0;
+    }
+    else if (IsKeyDown(KEY_UP)) {
+      direction.y = -1;
+      direction.x = 0;
+    }
 
-    // Draw
-    //----------------------------------------------------------------------------------
     BeginDrawing();
-
     ClearBackground(RAYWHITE);
-
-    BeginMode3D(camera);
-
-    DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-    DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-    DrawGrid(10, 1.0f);
-
-    EndMode3D();
-
-    DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
-    DrawFPS(10, 10);
-
+    BeginMode2D(camera);
+    moveSnake(positions, direction);
+    drawSnake(positions);
+    EndMode2D();
     EndDrawing();
-    //----------------------------------------------------------------------------------
   }
 
   // De-Initialization
   //--------------------------------------------------------------------------------------
-  CloseWindow(); // Close window and OpenGL context
+  CloseWindow();  // Close window and OpenGL context
   //--------------------------------------------------------------------------------------
 
   return 0;
+}
+
+void moveSnake(vector<Vector2> &snake, Vector2 direction) {
+  int head = 0;
+  int tail = snake.size();
+  Vector2 prev = {snake[0].x, snake[0].y};
+
+  for (int i = head + 1; i < tail; i++) {
+    Vector2 current = {snake[i].x, snake[i].y};
+    snake[i].x = prev.x;
+    snake[i].y = prev.y;
+    prev = current;
+  }
+  snake[head].x += direction.x * SNAKE_SCALE;
+  snake[head].y += direction.y * SNAKE_SCALE;
+}
+
+void drawSnake(vector<Vector2> positions) {
+  for (int i; i < positions.size(); i++) {
+    DrawRectangle(positions[i].x, positions[i].y, SNAKE_SCALE, SNAKE_SCALE, MAROON);
+  }
 }
