d82464870c7ed657675df915a5be6d4deb1c4c6e
Shidil Eringa <shidil@live.com>
2021-05-23 08:16:04 +0530

    Added hello raylib

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,37 +1,14 @@
-/*******************************************************************************************
- *
- *   raylib [core] example - Basic 3d example
- *
- *   Welcome to raylib!
- *
- *   To compile example, just press F5.
- *   Note that compiled executable is placed in the same folder as .c file
- *
- *   You can find all basic examples on C:\raylib\raylib\examples folder or
- *   raylib official webpage: www.raylib.com
- *
- *   Enjoy using raylib. :)
- *
- *   This example has been created using raylib 1.0 (www.raylib.com)
- *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h
- *for details)
- *
- *   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include <raylib.h>
 
 #include <vector>
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define SNAKE_SCALE 5
 using namespace std;
 
 void drawSnake(vector<Vector2>);
 void moveSnake(vector<Vector2> &currentSnake, Vector2 direction);
-
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 450;
-const int SNAKE_SCALE = 5;
 
 int main() {
   // Initialization
